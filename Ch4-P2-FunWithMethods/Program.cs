cd8df0b2e147b3fe3ed3206dbfd0e0f0c8dd70df
Ammar The Trainer <Ammarthetrainer@gmail.com>
2018-07-11 06:36:28 -0700

    comit from lec

--- Ch4-P2-FunWithMethods/Program.cs
+++ Ch4-P2-FunWithMethods/Program.cs
@@ -8,9 +8,21 @@ namespace Ch4_P2_FunWithMethods
 {
     class Program
     {
+        //public static int Add(int a , int b)
+        //{
+        //    return a + b;
+        //}
+
+        public static int Add(int a, int b) => a + b;
+        
+
         static void Main(string[] args)
         {
+            Console.WriteLine(" Lecture 4 - Project 2 - Methods ");
 
+
+
+            Console.ReadLine();
         }
     }
 }
